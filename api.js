551124769bbe1e438a3b5131782689c8da4ba0bc
obwansan <kob123@hotmail.co.uk>
2018-04-13 16:14:01 +0100

    get array of objects - need to get specific fields

--- api.js
+++ api.js
@@ -2,16 +2,16 @@
 *
 *  Create component using HTML and CSS
 *  Get all the repos returned by get request
-*  Pull top 3 JS repos out of get response and put in an array
+*  Pull top 3 JS repos (objects) out of get response and put in an array
+*  Use object notation to access required values from repo object
 *  Use map to loop over 3 repos and print them out in html component
 * */
 
-fetch('https://api.github.com/search/repositories?q=stars:>50000+language:javascript+sort:stars-desc',
-    {   method: 'get',
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
+// Get all the repos returned by get request
+fetch('https://api.github.com/search/repositories?q=stars:>50000+language:javascript+sort:stars-desc')
     .then(function (data) {
-        console.log(data)
-    })
+        return data.json()
+    }).then(function(data){
+        let top3Repos = data.items.slice(0,3)
+        console.log(top3Repos)
+})
